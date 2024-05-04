229792082ad95f37aca7577544a102686b7d11c8
Luis Tovar <ltovaralc@gmail.com>
2024-05-03 19:40:45 -0600

    add style header

--- src/globalComponents/header/header.jsx
+++ src/globalComponents/header/header.jsx
@@ -4,7 +4,7 @@ import "./header.css"
 const Header = () => {
     return(
         <header className="header">
-            <img src="../../../img/nube.png" alt="" className="cloud-logo"/>
+            <img src="../../../img/cloudLogo.png" alt="" className="cloud-logo"/>
             <SearchBar/>
             <div className="header-buttons__container">
                 <button className="header-button"><img src="../../../../img/usuario.png" className="header-button__icon" /></button>
